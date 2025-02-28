cd6c704b4065e85edd4aa09ece9e87b73fb32935
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:46:11 +0530

    javascript Qus3.js

--- javascript Qus3.js
+++ javascript Qus3.js
@@ -1,6 +1,5 @@
-3.//Write a program which enter any integer then check and print, whether it is odd no. or even no.
-
-let number = 20; 
+3.Write a program which enter any integer then check and print, whether it is odd no. or even no.
+Solve:-let number = 20; 
 if (number % 2 === 0) {
     console.log("Even");
 } else {
