8b1c8e7639d19e435b72b9c8b0177636eb67dc41
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:45:36 +0530

    javascript Qus2.js

--- javascript Qus2.js
+++ javascript Qus2.js
@@ -1,6 +1,5 @@
-2.//Write a java script program, which enters marks in three subject of a students and calculate its average. if the average is 33 or more than print pass, otherwise print fai
-
-let subject1 = 40; 
+2.Write a java script program, which enters marks in three subject of a students and calculate its average. if the average is 33 or more than print pass, otherwise print fai
+Solve:-let subject1 = 40; 
 let subject2 = 35; 
 let subject3 = 30; 
 let average = (subject1 + subject2 + subject3) / 3;
