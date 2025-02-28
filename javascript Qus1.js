9c9dd4af59c9ebe62d9a333464ef78ef25c6932b
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:44:47 +0530

    javascript Qus1.js

--- javascript Qus1.js
+++ javascript Qus1.js
@@ -1,6 +1,5 @@
 Qus:- 1 Write a program which enter the age of a person and if the age is 18 or more than 18 then print you are adult, otherwise print you are minor.
-
-let age = 20;  
+Solve:- let age = 20;  
 if (age >= 18) {
     console.log("You are an adult.");
 } else {
