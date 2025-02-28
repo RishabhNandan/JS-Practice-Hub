fadddb8ba2e4fc2801527ce6f48a602ba9135c32
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:46:50 +0530

    javascript Qus4.js

--- javascript Qus4.js
+++ javascript Qus4.js
@@ -1,6 +1,5 @@
-4.//Write a program which enter any integer and if it is less than 5 than calculation and print its cube otherwise calculate and prints its square.
-
-let number = 4; 
+4.Write a program which enter any integer and if it is less than 5 than calculation and print its cube otherwise calculate and prints its square.
+Solve:-let number = 4; 
 if (number < 5) {
     let cube = number * number * number; 
     console.log("Cube:", cube);
